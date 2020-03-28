74d1565a9b19fee518ec5a22acdcdfd1a09fb4a3
Edgar Ramirez <edgartoala@gmail.com>
2020-03-27 20:54:32 -0600

    update project

--- js/map.js
+++ js/map.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+    var states = ["Aguascalientes","Baja California","Baja California Sur","Campeche","Coahuila","Colima","Chiapas","Chihuahua","Cdmx","Durango","Guanajuato","Guerrero","Hidalgo","Jalisco","México","Michoacán","Morelos","Nayarit","Nuevo León","Oaxaca","Puebla","Querétaro","Quintana Roo","San Luis Potosí","Sinaloa","Sonora","Tabasco","Tamaulipas","Tlaxcala","Veracruz","Yucatán","Zacatecas"];
     $('.points').click(function() {
         var id = $(this).attr('id');
 
-        console.log(id);
-        alert(id);
+        var element = $(this).first();
+        var position = element.position();
+        console.log("left: " + position.left + ", top: " + position.top);
+
+        var styles = {
+            top : (position.top - 80),
+            left: (position.left + 20)
+        };
+        $('.state').text(states[(id-1)]);
+        $('.bussiness-card').removeClass('hide').css(styles);
+        
+        // alert(id);
+    });
+
+    $('.btn-access').click(function() {
+        location.href = "products.html";
     });
 });
